5428abae165898bd3cbf3a38c243905e56c19157
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2013-04-28 21:59:49 +0400

    #16: Add logger
    Fixed UnitTests

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,5 +1,8 @@
 // Copyright (c) 2013 Alexander Ignatyev. All rights reserved.
 
+#include <g2log.h>
+#include <g2logworker.h>
+
 #include <cppunit/TestRunner.h>
 #include <cppunit/TestResult.h>
 #include <cppunit/TestResultCollector.h>
@@ -9,6 +12,9 @@
 #include "teamcity_cppunit.h"
 
 int main(int argc, char *argv[]) {
+    g2LogWorker g2log("optimer-tests", "");
+    g2::initializeLogging(&g2log);
+
     CppUnit::TestResult controller;
     CppUnit::TestResultCollector result;
     controller.addListener(&result);
