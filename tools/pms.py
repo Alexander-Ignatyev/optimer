82f245f879383e4310662acf1a3f8a69f6f98300
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2013-05-03 17:35:27 +0400

    #10: Performance Measurement System (PMS). Added output to file, N runnings each task.

--- tools/pms.py
+++ tools/pms.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 #
 # Copyright (c) 2013 Alexander Ignatyev. All rights reserved.
+#
+# Set parameters in function main()
 
 import os
 import sys
 import time
+import datetime
 import subprocess
 from copy import copy
 from tempfile import NamedTemporaryFile
@@ -27,11 +30,14 @@ class RunningResult(object):
     def __init__(self, process):
         timer = Timer()
         self.stdout, self.stderr = process.communicate()
-        self.runnimg_time = timer.elapsed()
+        self.running_time = timer.elapsed()
         self.return_code = process.returncode
 
     def __str__(self):
-        return 'return: {0}, total time: {1}'.format(self.return_code, self.runnimg_time)
+        return 'return: {0}, total time: {1}'.format(self.return_code, self.running_time)
+
+    def __repr__(self):
+        return '<<return: {0}, total time: {1}>>'.format(self.return_code, self.running_time)
 
 def run_task(args):
     process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -39,6 +45,7 @@ def run_task(args):
     return RunningResult(process)
 
 class ConfigParams(object):
+    default_valuations = ['serial', 'parallel-lock']
     default_containers = ['lifo', 'priority']
     default_schedulers = ['giving', 'requesting']
     default_num_threads = [4]
@@ -46,11 +53,17 @@ class ConfigParams(object):
     default_num_maximum_nodes = [80]
     
     def __init__(self
+        , valuations = None
         , containers = None
         , schedulers = None
         , num_threads = None
         , num_minimum_nodes = None
         , num_maximum_nodes = None):
+        if valuations:
+            self.valuations = valuations
+        else:
+            self.valuations = ConfigParams.default_valuations
+
         if containers:
             self.containers = containers
         else:
@@ -86,8 +99,12 @@ class Config(object):
         config['general'] = general
 
         self.config_files = {}
-        self.__generate_serial_tasks(config)
-        self.__generate_parallel_lock_tasks(config)
+
+        for valuation in self.params.valuations:
+            if valuation == 'serial':
+                self.__generate_serial_tasks(config)
+            elif valuation == 'parallel-lock':
+                self.__generate_parallel_lock_tasks(config)
 
         return self.config_files
 
@@ -150,25 +167,60 @@ class Config(object):
         return filename
 
 def main():
+    # set params section
     module = 'build_clang++_release/atsp'
-    
+    problem_path = 'data/ftv38.atsp'
+    num_runs = 2
+
     params = ConfigParams()
+    params.valuations = ['serial', 'parallel-lock']
     params.schedulers = ['requesting']
     params.num_threads = [2, 4, 8]
     params.num_minimum_nodes = [4, 8]
     params.num_maximum_nodes = [10, 20, 40, 80]
+    # end set params section
 
+    print 'generating tasks...'
     config = Config()
-    config_files = config.generate_tasks(params, 'data/ftv38.atsp')
-    for task in sorted(config_files.iterkeys()):
-        filename = config_files[task]
-        print task, 
-        res = run_task([module, filename])
-        print res
-        if res.return_code != 0:
-            print res.stdout
-            print res.stderr
-        os.unlink(filename)
+    config_files = config.generate_tasks(params, problem_path)
+
+    num = 0
+    num_configs = len(config_files)
+    all_runs = {}
+    for task_name in sorted(config_files.iterkeys()):
+        num += 1
+        filename = config_files[task_name]
+        print '[{0: 3}/{1}] '.format(num, num_configs), 'running ', task_name, '...'
+        runs = []
+        for _ in xrange(num_runs):
+            res = run_task([module, filename])
+            runs.append(res)
+            if res.return_code != 0:
+                print 'stdout: ['+res.stdout+']'
+                print 'stderr: ['+res.stderr+']'
+        all_runs[task_name] = runs
+
+    print 'preparing results...'
+    all_avg_times = {}
+    for task_name, runs in all_runs.iteritems():
+        avg_time = reduce(lambda sum, run: sum + run.running_time, runs, 0.0) / len(runs)
+        all_avg_times[task_name] = avg_time
+
+    result_filename = 'pms_result.'+datetime.datetime.now().strftime('%Y%m%d-%H%M%S')+'.txt'
+    print 'saving results to', result_filename, '...'
+    lines = []
+    lines.append('Average results:\n')
+    for task_name in sorted(all_avg_times.iterkeys()):
+        lines.append('{0} {1}\n'.format(task_name, all_avg_times[task_name]))
+
+    lines.append('\nResults:\n')
+    for task_name in sorted(all_runs.iterkeys()):
+        lines.append('{0} {1}\n'.format(task_name, all_runs[task_name]))
+
+    with open(result_filename, 'w') as f:
+        f.writelines(lines)
+
+    print 'done'
 
 if __name__ == '__main__':
     main()
