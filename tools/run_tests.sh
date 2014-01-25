c6e9c116bca2585de6bc3acf3436f5d74d7d70d1
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2014-01-26 00:49:01 +0400

    #46: Improved building time for run_tests.sh

--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
-# Copyright (c) 2013 Alexander Ignatyev. All rights reserved.
+# Copyright (c) 2013-2014 Alexander Ignatyev. All rights reserved.
 
 # Run from directory that contains CMakeLists.txt
-# Usage: run_tests.sh <COMPILER>
+# Usage: run_tests.sh <COMPILER> <-c>
+# use -c for clean build
 
-if [ "$1" != "" ];
-then
-    export CXX=$1
+if [ "$1" != "" ]; then
+    CXX=$1
 fi
 
-export BUILD_PATH=build_$CXX
+NUMBER_OF_CORES=1
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    NUMBER_OF_CORES=$(sysctl hw.ncpu | awk '{print $2}')
+fi
 
-mkdir $BUILD_PATH
+BUILD_PATH=build_$CXX
+
+if [[ ("$2" == "-c") && (-e $BUILD_PATH) ]]; then
+    rm -rf $BUILD_PATH
+fi
+
+if [ ! -e $BUILD_PATH ]; then
+    mkdir $BUILD_PATH
+fi
+
 rm -f $BUILD_PATH/optimer-tests
 cd $BUILD_PATH
 cmake -DCMAKE_BUILD_TYPE=Release ..
-make
+make -j $NUMBER_OF_CORES
 cd ../
 ./$BUILD_PATH/optimer-tests
